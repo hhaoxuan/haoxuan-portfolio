349cfceda6dedf19694c65983c58b6f95504f3d3
hhaoxuan <huanghaoxuan@gmail.com>
2026-04-23 19:24:35 -0700

    v3.3 tci & interactivity update

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll(".fade-in");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.4
+});
+elements.forEach(el => observer.observe(el));
